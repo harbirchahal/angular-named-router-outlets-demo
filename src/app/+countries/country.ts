dbf71937c4e278fc019492004d766edb04f0ecb4
harbirchahal <harbirchahal@hotmail.com>
2018-11-14 23:55:56 +0530

    update country model

--- src/app/+countries/country.ts
+++ src/app/+countries/country.ts
@@ -3,5 +3,18 @@ export interface Country {
   name?: string;
   nativeName?: string;
   flag?: string;
+  capital?: string;
+  alpha2Code?: string;
+  alpha3Code?: string;
+  callingCodes?: string[];
+  currencies?: {
+    code?: string;
+    name?: string;
+    symbol?: string;
+  }[];
+  region?: string;
+  subregion?: string;
+  area?: number;
+  latlng?: number[];
   _lookup?: string;
 }
